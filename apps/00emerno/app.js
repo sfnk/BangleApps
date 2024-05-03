b7a4d573a631d742d4c795a3055c94087da7f266
sfnk <46817226+sfnk@users.noreply.github.com>
2024-05-03 22:54:02 +0200

    trying

--- apps/00emerno/app.js
+++ apps/00emerno/app.js
@@ -30,312 +30,303 @@ let drawTimeout;
 let draw;
 let isAlerting = false;
 let alertCancelInterval, clearAlertCancelInterval, clearAlertNotCancelled;
-{
-    function setDrawClock(){
-        console.log("setDrawClock");
-        draw = function() {
-            var x = g.getWidth() / 2;
-            var y = g.getHeight() / 2;
-            g.reset().clearRect(Bangle.appRect); // clear whole background (w/o widgets)
-            var date = new Date();
-            var timeStr = require("locale").time(date, 1); // Hour and minute
-            g.setFontAlign(0, 0).setFont("Anton").drawString(timeStr, x, y);
-            // Show date and day of week
-            var dateStr = require("locale").date(date, 0).toUpperCase()+"\n"+
-                require("locale").dow(date, 0).toUpperCase();
-            g.setFontAlign(0, 0).setFont("6x8", 2).drawString(dateStr, x, y+48);
 
-            g.setFontAlign(0, 0).setFont("4x6", 2).drawString(lastUILog, x, y+80);
+function setDrawClock(){
+    console.log("setDrawClock");
+    draw = function() {
+        var x = g.getWidth() / 2;
+        var y = g.getHeight() / 2;
+        g.reset().clearRect(Bangle.appRect); // clear whole background (w/o widgets)
+        var date = new Date();
+        var timeStr = require("locale").time(date, 1); // Hour and minute
+        g.setFontAlign(0, 0).setFont("Anton").drawString(timeStr, x, y);
+        // Show date and day of week
+        var dateStr = require("locale").date(date, 0).toUpperCase()+"\n"+
+            require("locale").dow(date, 0).toUpperCase();
+        g.setFontAlign(0, 0).setFont("6x8", 2).drawString(dateStr, x, y+48);
 
-            // queue next draw
-            if (drawTimeout) clearTimeout(drawTimeout);
-            drawTimeout = setTimeout(function() {
-                drawTimeout = undefined;
-                draw();
-            }, 60000 - (Date.now() % 60000));
-        };
-        draw();
-    }
+        g.setFontAlign(0, 0).setFont("4x6", 2).drawString(lastUILog, x, y+80);
 
-    function setDrawAlert(){
-        console.log("setDrawAlert");
-        draw = function() {
-            var x = g.getWidth() / 2;
-            var y = g.getHeight() / 2;
-            g.reset().clearRect(Bangle.appRect); // clear whole background (w/o widgets)
-            //g.setBgColor(155,0,0);
-            g.setColor(155,0,0).fillRect(Bangle.appRect);
-            g.setColor(155,155,155);
-            g.setFontAlign(0, 0).setFont("Vector", 40).drawString("Sturz?", x, y);
+        // queue next draw
+        if (drawTimeout) clearTimeout(drawTimeout);
+        drawTimeout = setTimeout(function() {
+            drawTimeout = undefined;
+            draw();
+        }, 60000 - (Date.now() % 60000));
+    };
+    draw();
+}
 
-            // queue next draw
-            if (drawTimeout) clearTimeout(drawTimeout);
-            drawTimeout = setTimeout(function() {
-                drawTimeout = undefined;
-                draw();
-            }, 1000 - (Date.now() % 1000));
-        };
-        draw();
-    }
+function setDrawAlert(){
+    console.log("setDrawAlert");
+    draw = function() {
+        var x = g.getWidth() / 2;
+        var y = g.getHeight() / 2;
+        g.reset().clearRect(Bangle.appRect); // clear whole background (w/o widgets)
+        //g.setBgColor(155,0,0);
+        g.setColor(155,0,0).fillRect(Bangle.appRect);
+        g.setColor(155,155,155);
+        g.setFontAlign(0, 0).setFont("Vector", 40).drawString("Sturz?", x, y);
 
-    function onAlertCancelled(){
-        console.log("onAlertCancelled");
-        isAlerting = false;
-        setDrawClock();
-    }
+        // queue next draw
+        if (drawTimeout) clearTimeout(drawTimeout);
+        drawTimeout = setTimeout(function() {
+            drawTimeout = undefined;
+            draw();
+        }, 1000 - (Date.now() % 1000));
+    };
+    draw();
+}
 
-    function onAlertNotCancelled(data) {
-        console.log("onAlertNotCancelled");
-        resetTimers();
-        if (connected) {
-            NRF.updateServices({
-                "123f0001-40c3-4cf3-9797-9a8703e32795": {
-                    "123f0002-40c3-4cf3-9797-9a8703e32795": {
-                        value: new Float32Array([Date.now(), data.diff, data.mag]).buffer,
-                        notify: true
-                    }
-                }
-            });
-        }
-        setDrawClock();
-    }
+function onAlertCancelled(){
+    console.log("onAlertCancelled");
+    isAlerting = false;
+    setDrawClock();
+}
 
-    function stepCount(){
-        if (connected) {
-            NRF.updateServices({
-                "123f0001-40c3-4cf3-9797-9a8703e32795": {
-                    "123f0004-40c3-4cf3-9797-9a8703e32795": {
-                        value : new Float32Array([Date.now(), Bangle.getStepCount()]).buffer,
-                        notify: true
-                    }
-                }
-            });
-        }
-    }
-
-    function batteryPercentage(){
-        if (connected) {
-            NRF.updateServices({
-                "123f0001-40c3-4cf3-9797-9a8703e32795": {
-                    "123f0005-40c3-4cf3-9797-9a8703e32795": {
-                        value : new Float32Array([Date.now(), E.getBattery()]).buffer,
-                        notify: true
-                    }
-                }
-            });
-        }
-    }
-
-    function onHRM(hrm){
-        hrmCountPoll = hrmCountPoll + 1;
-        //console.log("hrm: " + hrm.bpm)
-        if (connected && hrmCountPoll > 100){
-            hrmCountPoll = 0;
-            NRF.updateServices({
-                "123f0001-40c3-4cf3-9797-9a8703e32795": {
-                    "123f0003-40c3-4cf3-9797-9a8703e32795": {
-                        value : new Float32Array([Date.now(), Math.round(hrm.bpm), hrm.confidence]).buffer,
-                        notify: true
-                    }
-                }
-            });
-        }
-    }
-
-    function onLongPress() {
-        console.log("onLongPress");
-        Bangle.buzz(1000, 1);
-        if (connected) {
-            NRF.updateServices({
-                "123f0001-40c3-4cf3-9797-9a8703e32795": {
-                    "123f0006-40c3-4cf3-9797-9a8703e32795": {
-                        value : new Float32Array([Date.now(), 1]).buffer,
-                        notify: true
-                    }
-                }
-            });
-        }
-    }
-
-    function resetTimers(){
-        console.log("resetTimers");
-        if(alertCancelInterval)
-            clearInterval(alertCancelInterval);
-        if(clearAlertCancelInterval)
-            clearTimeout(clearAlertCancelInterval);
-        if(clearAlertNotCancelled)
-            clearTimeout(clearAlertNotCancelled);
-
-        alertCancelInterval = null;
-        clearAlertCancelInterval = null;
-        clearAlertNotCancelled = null;
-        isAlerting = false;
-    }
-
-    function onButtonPressed(n){
-        console.log("onButtonPressed");
-        if(isAlerting) {
-            resetTimers();
-            onAlertCancelled();
-        }
-    }
-
-    function alert(){
-        Bangle.buzz(150, 1);
-    }
-
-    function onAlert(data){
-        console.log("onAlert");
-        isAlerting = true;
-        setDrawAlert();
-        alertCancelInterval = setInterval(alert, 300);
-        clearAlertCancelInterval = setTimeout(clearInterval, 10000, alertCancelInterval);
-        clearAlertNotCancelled = setTimeout(onAlertNotCancelled, 10000, data);
-    }
-
-    var hrmCountPoll = 0;
-    var batteryInterval, connected = false;
-
-
-    function onAccel(d) {
-        //nsole.log("diff: " + d.diff + " mag: " + d.mag);
-        if(!isAlerting){
-            if(d.diff > 1.0 && d.mag > 1.8) {
-                console.log("ON ALERT: " + d.mag)
-                setTimeout(onAlert, 0, d);
-            }
-        }
-    }
-
-    function onBondErased(){
-        uiLog("bonds erased");
-    }
-
-    function uiLog(text){
-        lastUILog = text;
-        draw();
-    }
-
-    function onInit() {
-        NRF.on('connect', function () { connected = true;
-            uiLog("connected");
-        });
-        NRF.on('disconnect', function () { connected = false;
-            uiLog("disconnected");
-            NRF.eraseBonds(onBondErased);
-        });
-
-        NRF.on('bond', function(status) {
-            uiLog("bond: " + status);
-            if(status == "success"){
-                connected = true;
-            } else {
-                connected = false;
-            }
-        });
-
-        NRF.on('advertising', function(isAdvertising) {
-            uiLog("advertising: " + isAdvertising);
-        });
-
-        NRF.setServices({
+function onAlertNotCancelled(data) {
+    console.log("onAlertNotCancelled");
+    resetTimers();
+    if (connected) {
+        NRF.updateServices({
             "123f0001-40c3-4cf3-9797-9a8703e32795": {
                 "123f0002-40c3-4cf3-9797-9a8703e32795": {
-                    notify: true,
-                    description: "accel data",
-                    value : new Float32Array([0 ,0, 0]).buffer,
-                },
-                "123f0003-40c3-4cf3-9797-9a8703e32795": {
-                    notify: true,
-                    description: "heart rate",
-                    value : new Float32Array([0, 0, 0]).buffer,
-                },
-                "123f0004-40c3-4cf3-9797-9a8703e32795": {
-                    notify: true,
-                    description: "steps count",
-                    value : new Float32Array([0, 0]).buffer
-                },
-                "123f0005-40c3-4cf3-9797-9a8703e32795": {
-                    notify: true,
-                    description: "battery percentage",
-                    value : new Float32Array([0, 0]).buffer
-                },
-                "123f0006-40c3-4cf3-9797-9a8703e32795": {
-                    notify: true,
-                    description: "notification for emergency button",
-                    value : new Float32Array([0, 0]).buffer
+                    value: new Float32Array([Date.now(), data.diff, data.mag]).buffer,
+                    notify: true
                 }
             }
         });
-        uiLog("Waiting..","Bluetooth Connection");
-        setInterval(stepCount, 30000);
-        setInterval(batteryPercentage, 5000);
-        Bangle.on('accel', onAccel);
-        Bangle.on('HRM-raw', onHRM);
-        Bangle.setHRMPower(1, "emerno");
     }
+    setDrawClock();
+}
 
-    let btnReleaseTimeout;
-
-    function onBtnReleased(e){
-        console.log("onBtnReleased");
-        if(btnReleaseTimeout)
-            clearTimeout(btnReleaseTimeout);
+function stepCount(){
+    if (connected) {
+        NRF.updateServices({
+            "123f0001-40c3-4cf3-9797-9a8703e32795": {
+                "123f0004-40c3-4cf3-9797-9a8703e32795": {
+                    value : new Float32Array([Date.now(), Bangle.getStepCount()]).buffer,
+                    notify: true
+                }
+            }
+        });
     }
+}
 
-    function onBtnPressed(e) {
-        console.log("onBtnPressed");
-        onButtonPressed();
-        if(BTN1.read()){
-            btnReleaseTimeout = setTimeout(onLongPress, 3000);
+function batteryPercentage(){
+    if (connected) {
+        NRF.updateServices({
+            "123f0001-40c3-4cf3-9797-9a8703e32795": {
+                "123f0005-40c3-4cf3-9797-9a8703e32795": {
+                    value : new Float32Array([Date.now(), E.getBattery()]).buffer,
+                    notify: true
+                }
+            }
+        });
+    }
+}
+
+function onHRM(hrm){
+    hrmCountPoll = hrmCountPoll + 1;
+    //console.log("hrm: " + hrm.bpm)
+    if (connected && hrmCountPoll > 100){
+        hrmCountPoll = 0;
+        NRF.updateServices({
+            "123f0001-40c3-4cf3-9797-9a8703e32795": {
+                "123f0003-40c3-4cf3-9797-9a8703e32795": {
+                    value : new Float32Array([Date.now(), Math.round(hrm.bpm), hrm.confidence]).buffer,
+                    notify: true
+                }
+            }
+        });
+    }
+}
+
+function onLongPress() {
+    console.log("onLongPress");
+    Bangle.buzz(1000, 1);
+    if (connected) {
+        NRF.updateServices({
+            "123f0001-40c3-4cf3-9797-9a8703e32795": {
+                "123f0006-40c3-4cf3-9797-9a8703e32795": {
+                    value : new Float32Array([Date.now(), 1]).buffer,
+                    notify: true
+                }
+            }
+        });
+    }
+}
+
+function resetTimers(){
+    console.log("resetTimers");
+    if(alertCancelInterval)
+        clearInterval(alertCancelInterval);
+    if(clearAlertCancelInterval)
+        clearTimeout(clearAlertCancelInterval);
+    if(clearAlertNotCancelled)
+        clearTimeout(clearAlertNotCancelled);
+
+    alertCancelInterval = null;
+    clearAlertCancelInterval = null;
+    clearAlertNotCancelled = null;
+    isAlerting = false;
+}
+
+function onButtonPressed(n){
+    console.log("onButtonPressed");
+    if(isAlerting) {
+        resetTimers();
+        onAlertCancelled();
+    }
+}
+
+function alert(){
+    Bangle.buzz(150, 1);
+}
+
+function onAlert(data){
+    console.log("onAlert");
+    isAlerting = true;
+    setDrawAlert();
+    alertCancelInterval = setInterval(alert, 300);
+    clearAlertCancelInterval = setTimeout(clearInterval, 10000, alertCancelInterval);
+    clearAlertNotCancelled = setTimeout(onAlertNotCancelled, 10000, data);
+}
+
+var hrmCountPoll = 0;
+var batteryInterval, connected = false;
+
+
+function onAccel(d) {
+    //nsole.log("diff: " + d.diff + " mag: " + d.mag);
+    if(!isAlerting){
+        if(d.diff > 1.0 && d.mag > 1.8) {
+            console.log("ON ALERT: " + d.mag)
+            setTimeout(onAlert, 0, d);
         }
     }
+}
 
-    setWatch(onBtnPressed,BTN1,{repeat:true, edge:'rising'});
-    setWatch(onBtnReleased,BTN1,{repeat:true, edge:'falling'});
+function onBondErased(){
+    uiLog("bonds erased");
+}
 
-    function initGraphics(){
-        //Bangle.setUI({
-        //    mode : "clock",
-        //    btn: onButtonPressed
-        //});
-        Bangle.loadWidgets();
-        setTimeout(Bangle.drawWidgets,0);
+function uiLog(text){
+    lastUILog = text;
+    draw();
+}
+
+function onInit() {
+    NRF.on('connect', function () { connected = true;
+        uiLog("connected");
+    });
+    NRF.on('disconnect', function () { connected = false;
+        uiLog("disconnected");
+        NRF.eraseBonds(onBondErased);
+    });
+
+    NRF.on('bond', function(status) {
+        uiLog("bond: " + status);
+        if(status == "success"){
+            connected = true;
+        } else {
+            connected = false;
+        }
+    });
+
+    NRF.on('advertising', function(isAdvertising) {
+        uiLog("advertising: " + isAdvertising);
+    });
+
+    NRF.setServices({
+        "123f0001-40c3-4cf3-9797-9a8703e32795": {
+            "123f0002-40c3-4cf3-9797-9a8703e32795": {
+                notify: true,
+                description: "accel data",
+                value : new Float32Array([0 ,0, 0]).buffer,
+            },
+            "123f0003-40c3-4cf3-9797-9a8703e32795": {
+                notify: true,
+                description: "heart rate",
+                value : new Float32Array([0, 0, 0]).buffer,
+            },
+            "123f0004-40c3-4cf3-9797-9a8703e32795": {
+                notify: true,
+                description: "steps count",
+                value : new Float32Array([0, 0]).buffer
+            },
+            "123f0005-40c3-4cf3-9797-9a8703e32795": {
+                notify: true,
+                description: "battery percentage",
+                value : new Float32Array([0, 0]).buffer
+            },
+            "123f0006-40c3-4cf3-9797-9a8703e32795": {
+                notify: true,
+                description: "notification for emergency button",
+                value : new Float32Array([0, 0]).buffer
+            }
+        }
+    });
+    uiLog("Waiting..","Bluetooth Connection");
+    setInterval(stepCount, 30000);
+    setInterval(batteryPercentage, 5000);
+    Bangle.on('accel', onAccel);
+    Bangle.on('HRM-raw', onHRM);
+    Bangle.setHRMPower(1, "emerno");
+}
+
+let btnReleaseTimeout;
+
+function onBtnReleased(e){
+    console.log("onBtnReleased");
+    if(btnReleaseTimeout)
+        clearTimeout(btnReleaseTimeout);
+}
+
+function onBtnPressed(e) {
+    console.log("onBtnPressed");
+    onButtonPressed();
+    if(BTN1.read()){
+        btnReleaseTimeout = setTimeout(onLongPress, 3000);
     }
-
-    function startApp(){
-        Bangle.setOptions({
-            lockTimeout: 0,
-            backlightTimeout: 10000,
-            btnLoadTimeout: 0,
-            hrmSportMode: -1
-        });
-
-        NRF.setTxPower(8);
-
-        var bootTimer = 0;
-        setInterval(function() {
-            if (BTN.read())
-                bootTimer++;
-            else bootTimer=0;
-
-            if (bootTimer<10) E.kickWatchdog();
-        }, 2000);
-
-        Bangle.setLocked(false);
-
-        setDrawClock();
-        onInit(); // set up our new services
-        initGraphics();
-    }
 }
 
-try {
-    eval("dec");
-} catch (e) {
-    startApp();
+setWatch(onBtnPressed,BTN1,{repeat:true, edge:'rising'});
+setWatch(onBtnReleased,BTN1,{repeat:true, edge:'falling'});
+
+function initGraphics(){
+    //Bangle.setUI({
+    //    mode : "clock",
+    //    btn: onButtonPressed
+    //});
+    Bangle.loadWidgets();
+    setTimeout(Bangle.drawWidgets,0);
 }
 
-function dec(){
-    return true;
+function startApp(){
+    Bangle.setOptions({
+        lockTimeout: 0,
+        backlightTimeout: 10000,
+        btnLoadTimeout: 0,
+        hrmSportMode: -1
+    });
+
+    NRF.setTxPower(8);
+
+    var bootTimer = 0;
+    setInterval(function() {
+        if (BTN.read())
+            bootTimer++;
+        else bootTimer=0;
+
+        if (bootTimer<10) E.kickWatchdog();
+    }, 2000);
+
+    Bangle.setLocked(false);
+
+    setDrawClock();
+    onInit(); // set up our new services
+    initGraphics();
 }
+
+startApp();
